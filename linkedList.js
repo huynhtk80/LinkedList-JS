04193b791b75f618b4e569197a7a110c42109ef9
Timothy H <huynh.tk80@gmail.com>
2023-07-20 13:28:08 -0600

    first commit

---
+++ linkedList.js
@@ -0,0 +1,60 @@
+export default class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  insertAtHead(data) {
+    const newNode = new LinkedListNode(data, this.head);
+    this.head = newNode;
+    this.length++;
+  }
+
+  getByIndex(index) {
+    if (index < 0 || index >= this.length) return null;
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  insertAtIndex(index, value) {
+    if (index < 0) return null;
+    if (index === 0) return this.insertAtHead(value);
+
+    let previousNode = this.getByIndex(index - 1);
+    if (previousNode == null) return null;
+
+    previousNode.next = new LinkedListNode(value, previousNode.next);
+  }
+
+  removeHead() {
+    this.head = this.head.next;
+    this.length--;
+  }
+  print() {
+    let output = "";
+    let currentNode = this.head;
+    while (currentNode) {
+      output = output + currentNode.value + " -> ";
+      currentNode = currentNode.next;
+    }
+    console.log(output + "null");
+  }
+}
+
+class LinkedListNode {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+LinkedList.fromValues = function (...values) {
+  const ll = new LinkedList();
+  for (let i = values.length - 1; i >= 0; i--) {
+    ll.insertAtHead(values[i]);
+  }
+  return ll;
+};
---
+++ linkedList.test.js
@@ -0,0 +1,81 @@
+import LinkedList from "./LinkedList.js";
+
+describe("#insertAtHead", () => {
+  test("it adds the element to the beginning of the list", () => {
+    const ll = new LinkedList();
+    ll.insertAtHead(10);
+    const oldhead = ll.head;
+    ll.insertAtHead(20);
+
+    expect(ll.head.value).toBe(20);
+    expect(ll.head.next).toBe(oldhead);
+    expect(ll.length).toBe(2);
+  });
+});
+
+describe("#getByIndex", () => {
+  describe("with index less than 0", () => {
+    test("it returns null", () => {
+      const ll = LinkedList.fromValues(10, 20);
+      expect(ll.getByIndex(-1)).toBeNull();
+    });
+  });
+  describe("with index greater than list length", () => {
+    test("it returns null", () => {
+      const ll = LinkedList.fromValues(10, 20);
+      expect(ll.getByIndex(5)).toBeNull();
+    });
+  });
+  describe("with index 0", () => {
+    test("it returns the head", () => {
+      const ll = LinkedList.fromValues(10, 20);
+      expect(ll.getByIndex(0).value).toBe(10);
+    });
+  });
+  describe("with index in the middle", () => {
+    test("it returns the head", () => {
+      const ll = LinkedList.fromValues(10, 20, 30, 40, 50);
+      expect(ll.getByIndex(3).value).toBe(40);
+    });
+  });
+});
+
+describe("#insertAtIndex", () => {
+  describe("with index less than 0", () => {
+    test("it returns null", () => {
+      const ll = LinkedList.fromValues(10, 20);
+      ll.insertAtIndex(-1, 30);
+      expect(ll.length).toBe(2);
+    });
+  });
+  describe("with index 0", () => {
+    test("insert at head", () => {
+      const ll = LinkedList.fromValues(10, 20);
+      ll.insertAtIndex(0, 30);
+      expect(ll.length).toBe(3);
+      expect(ll.head.value).toBe(30);
+      expect(ll.head.next.value).toBe(10);
+    });
+  });
+
+  describe("with index in the middle", () => {
+    test("insert at given index", () => {
+      const ll = LinkedList.fromValues(10, 20, 30, 40, 50);
+      ll.insertAtIndex(2, 60);
+      expect(ll.length).toBe(5);
+      expect(ll.getByIndex(2).value).toBe(60);
+      expect(ll.getByIndex(2).next.value).toBe(30);
+    });
+  });
+});
+
+describe("#removeHead", () => {
+  describe("removes the head", () => {
+    test("it returns null", () => {
+      const ll = LinkedList.fromValues(10, 20, 30, 40, 50);
+      ll.removeHead();
+      expect(ll.length).toBe(4);
+      expect(ll.head.value).toBe(20);
+    });
+  });
+});
